a039f0bce73c81493e5dd32520103ab915932543
木内 亜美 <kiuchi@skyarch.jp>
2024-06-12 01:30:36 +0900

    DB修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,75 +10,85 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_240_504_122_145) do
-  create_table 'movies', charset: 'utf8mb4', collation: 'utf8mb4_0900_ai_ci', force: :cascade do |t|
-    t.string 'name', limit: 160, null: false, comment: '映画のタイトル。邦題・洋題は一旦考えなくてOK'
-    t.string 'year', limit: 45, comment: '公開年'
-    t.text 'description', comment: '映画の説明文'
-    t.string 'image_url', limit: 150, comment: '映画のポスター画像が格納されているURL'
-    t.boolean 'is_showing', null: false, comment: '上映中かどうか'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.bigint 'screen_id'
-    t.index ['name'], name: 'index_movies_on_name'
-    t.index ['screen_id'], name: 'index_movies_on_screen_id'
+ActiveRecord::Schema.define(version: 2024_06_04_165333) do
+
+  create_table "movies", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name", limit: 160, null: false, comment: "映画のタイトル。邦題・洋題は一旦考えなくてOK"
+    t.string "year", limit: 45, comment: "公開年"
+    t.text "description", comment: "映画の説明文"
+    t.string "image_url", limit: 150, comment: "映画のポスター画像が格納されているURL"
+    t.boolean "is_showing", null: false, comment: "上映中かどうか"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "screen_id"
+    t.index ["name"], name: "index_movies_on_name"
+    t.index ["screen_id"], name: "index_movies_on_screen_id"
   end
 
-  create_table 'reservations', charset: 'utf8mb4', collation: 'utf8mb4_0900_ai_ci', force: :cascade do |t|
-    t.date 'date', null: false
-    t.bigint 'schedule_id', null: false
-    t.bigint 'sheet_id', null: false
-    t.string 'email', null: false, comment: '予約者メールアドレス'
-    t.string 'name', null: false, comment: '予約者名'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.bigint 'screen_id'
-    t.bigint 'user_id', null: false
-    t.index %w[date schedule_id sheet_id], name: 'reservation_schedule_sheet_unique', unique: true
-    t.index ['schedule_id'], name: 'reservation_schedule_id_idx'
-    t.index ['screen_id'], name: 'index_reservations_on_screen_id'
-    t.index ['sheet_id'], name: 'reservation_sheet_id_idx'
-    t.index ['user_id'], name: 'index_reservations_on_user_id'
+  create_table "reservations", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.date "date", null: false
+    t.bigint "schedule_id", null: false
+    t.bigint "sheet_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "screen_id"
+    t.bigint "user_id", null: false
+    t.index ["date", "schedule_id", "sheet_id"], name: "reservation_schedule_sheet_unique", unique: true
+    t.index ["schedule_id"], name: "reservation_schedule_id_idx"
+    t.index ["screen_id"], name: "index_reservations_on_screen_id"
+    t.index ["sheet_id"], name: "reservation_sheet_id_idx"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
   end
 
-  create_table 'schedules', charset: 'utf8mb4', collation: 'utf8mb4_0900_ai_ci', force: :cascade do |t|
-    t.bigint 'movie_id', null: false
-    t.datetime 'start_time', null: false, comment: '上映開始時刻'
-    t.datetime 'end_time', null: false, comment: '上映終了時刻'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['movie_id'], name: 'index_schedules_on_movie_id'
+  create_table "schedules", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "movie_id", null: false
+    t.datetime "start_time", null: false, comment: "上映開始時刻"
+    t.datetime "end_time", null: false, comment: "上映終了時刻"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["movie_id"], name: "index_schedules_on_movie_id"
   end
 
-  create_table 'screens', charset: 'utf8mb4', collation: 'utf8mb4_0900_ai_ci', force: :cascade do |t|
-    t.integer 'screen_number', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "screens", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.integer "screen_number", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "theater_id", null: false
+    t.index ["theater_id"], name: "index_screens_on_theater_id"
   end
 
-  create_table 'sheets', charset: 'utf8mb4', collation: 'utf8mb4_0900_ai_ci', force: :cascade do |t|
-    t.integer 'column', limit: 1, null: false
-    t.string 'row', limit: 1, null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "sheets", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.integer "column", limit: 1, null: false
+    t.string "row", limit: 1, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table 'users', charset: 'utf8mb4', collation: 'utf8mb4_0900_ai_ci', force: :cascade do |t|
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.string 'name'
-    t.index ['email'], name: 'index_users_on_email', unique: true
-    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+  create_table "theaters", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name", limit: 160, null: false, comment: "劇場名"
+    t.string "address", limit: 160, null: false, comment: "劇場の住所"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["address"], name: "index_theaters_on_address"
+    t.index ["name"], name: "index_theaters_on_name"
   end
 
-  add_foreign_key 'movies', 'screens'
-  add_foreign_key 'reservations', 'schedules'
-  add_foreign_key 'reservations', 'screens'
-  add_foreign_key 'reservations', 'sheets'
-  add_foreign_key 'schedules', 'movies'
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "movies", "screens"
+  add_foreign_key "reservations", "schedules"
+  add_foreign_key "reservations", "screens"
+  add_foreign_key "reservations", "sheets"
+  add_foreign_key "schedules", "movies"
 end
