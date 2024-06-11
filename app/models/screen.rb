d35630bea533a8825d69e34509c52e279ac95e44
木内 亜美 <kiuchi@skyarch.jp>
2024-06-12 01:35:10 +0900

    theaterモデル新規作成に伴い各モデル見直し

--- app/models/screen.rb
+++ app/models/screen.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
-# Screen モデル　各スクリーンマスタ
+# Screen モデル　各スクリーンのマスタデータ
 class Screen < ApplicationRecord
-  validates :screen_number, presence: true
+  validates :screen_number, presence: true, uniqueness: { scope: %i[theater_id] }
+
+  belongs_to :theater
+
+  def screen_theater_name
+    "#{theater.name}：#{screen_number}"
+  end
 end
