e0401c103000d04713619689e6bc1426d2864465
木内 亜美 <kiuchi@skyarch.jp>
2024-06-23 23:10:52 +0900

    スケジュール登録機能作成

--- app/controllers/admin/schedules_controller.rb
+++ app/controllers/admin/schedules_controller.rb
@@ -11,6 +11,41 @@ module Admin
       @schedule = Schedule.find(params[:id])
     end
 
+    def create
+      start_date = Date.parse(schedule_create_params[:start_date])
+      end_date = Date.parse(schedule_create_params[:end_date])
+      days_of_week = schedule_create_params[:days_of_week].map(&:to_i)
+
+      schedules = []
+
+      if start_date > end_date
+        flash[:alert] = '終了日は開始日より後の日付にしてください。'
+        render :new and return
+      end
+
+      # 指定した期間、曜日に当てはまる日付でレコードを作成
+      (start_date..end_date).each do |date|
+        if days_of_week.include?(date.wday)
+          schedules << Schedule.new(
+        movie_id: schedule_create_params[:movie_id],
+        screen_id: schedule_create_params[:screen_id],
+        date: date,
+        start_time: schedule_create_params[:start_time],
+        end_time: schedule_create_params[:end_time]
+      )
+        end
+      end
+
+      if schedules.all?(&:valid?)
+        schedules.each(&:save!)
+        redirect_to admin_schedules_path, notice: 'スケジュールが作成されました'
+      else
+        error_messages = schedules.map { |s| s.errors.full_messages }.flatten.uniq
+        flash[:alert] = "スケジュールの作成に失敗しました: #{error_messages.join(', ')}"
+        render :new
+      end
+    end
+
     def edit
       @schedule = Schedule.find(params[:id])
     end
@@ -20,6 +55,7 @@ module Admin
       if @schedule.update(schedule_params)
         redirect_to admin_schedule_path(@schedule), notice: '上映スケジュールを更新しました。'
       else
+        flash[:alert] = @schedule.errors.full_messages.join(', ')
         render :edit
       end
     end
@@ -34,6 +70,18 @@ module Admin
 
     def schedule_params
       params.require(:schedule).permit(:start_time, :end_time, :date, :screen_id)
+    end
+
+    def schedule_create_params
+      {
+        movie_id: params[:movie_id],
+        screen_id: params[:screen_id],
+        start_date: params[:start_date],
+        end_date: params[:end_date],
+        start_time: params[:start_time],
+        end_time: params[:end_time],
+        days_of_week: params[:days_of_week]
+      }
     end
   end
 end
